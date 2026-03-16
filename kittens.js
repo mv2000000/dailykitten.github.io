75bb2c9d288735e5ef516a03795ecd402d0b69c6
mv2000000 <mavarghese@albany.edu>
2026-03-15 21:58:00 -0400

    Update kittens.js

--- kittens.js
+++ kittens.js
@@ -18,3 +18,40 @@ function transformKitten(array){
 	let randomNum = Math.floor(Math.random() * array.length); 
 	image.setAttribute("src", array[randomNum]);
 }
+
+document.getElementById("form").addEventListener("submit",(e) => {
+    e.preventDefault()
+
+    let key = document.getElementById("fav-kitten").value
+
+    if (key =="") {
+        alert("Please enter all the fields.")
+    }
+    else{
+        setCookie(key, 1)
+    }
+})
+
+function setCookie(key,time){
+    let d = new Date()
+
+    d.setTime(d.getTime() + (time*24*60*60*1000))
+
+    let expires ="expires" + d.toUTCString()
+
+    document.cookie= `${key};${expires};path="/"`
+
+    window.location.reload()
+
+}
+
+function getCookies(){
+    let cookies=document.cookie.split(";")
+    let res;
+
+    for (Let i=1; i<cookies.length; i++){
+        res+= i+ "-" + cookies [i-1 + "<br>"]
+    }
+
+    document.getElementById("result").innerHTML = res
+}
